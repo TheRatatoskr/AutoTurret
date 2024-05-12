d6901e26cd9775f832f3575557589597fdf652dd
TheRatatoskr <joseph.ewert@gmail.com>
2024-05-12 14:46:35 -0500

    added room enviroment

--- Assets/Scripts/AI/Spawner.cs
+++ Assets/Scripts/AI/Spawner.cs
@@ -7,14 +7,20 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject _enemyToSpawn;
     [SerializeField] private GameObject _player;
 
+    //update to use real time instead of frames plox
     [SerializeField] private float spawnWaitTime = 10f;
 
-    private float _spawnTimer = 0;
+    //currently three but use round robin plox
+    [SerializeField] private List<Transform> spawnPoints;
+    private int spawnPointIndex = 0;
+
+    [Header("**Debug Only**")]
+    [SerializeField] private float _spawnTimer = 0;
 
     // Update is called once per frame
     void Update()
     {
-        _spawnTimer++;
+        _spawnTimer += Time.deltaTime;
         SpawnDood();
     }
 
@@ -22,9 +28,18 @@ public class Spawner : MonoBehaviour
     {
         if(_spawnTimer > spawnWaitTime)
         {
-            GameObject spawnedDood = Instantiate(_enemyToSpawn, transform.position, Quaternion.identity);
+            GameObject spawnedDood = Instantiate(_enemyToSpawn, spawnPoints[spawnPointIndex].transform.position, Quaternion.identity);
 
+            //assigns player destination to object
+            //will need to update AI to read as player instead of target for the door
             spawnedDood.GetComponent<AIBasicMovement>().TakeWayPoint(_player.transform);
+
+            //increase index, and if index is greater than count, then go back to 0
+            spawnPointIndex++;
+            if (spawnPointIndex > spawnPoints.Count -1) 
+            { 
+                spawnPointIndex = 0; 
+            }
 
             _spawnTimer = 0;
         }
